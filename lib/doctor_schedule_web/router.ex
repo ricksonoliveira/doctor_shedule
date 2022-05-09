f626a48b9f119e3744fa1caa959dc6a286593351
ricksonoliveira <riksongleison471@gmail.com>
2022-05-09 13:42:51 -0300

    feat: ignoring live dashboard

--- lib/doctor_schedule_web/router.ex
+++ lib/doctor_schedule_web/router.ex
@@ -52,6 +52,7 @@ defmodule DoctorScheduleWeb.Router do
   # If your application does not have an admins-only section yet,
   # you can use Plug.BasicAuth to set up some basic authentication
   # as long as you are also using SSL (which you should anyway).
+  # coveralls-ignore-start
   if Mix.env() in [:dev, :test] do
     import Phoenix.LiveDashboard.Router
 
@@ -61,6 +62,8 @@ defmodule DoctorScheduleWeb.Router do
       live_dashboard "/dashboard", metrics: DoctorScheduleWeb.Telemetry
     end
   end
+
+  # coveralls-ignore-stop
 
   # Enables the Swoosh mailbox preview in development.
   #
